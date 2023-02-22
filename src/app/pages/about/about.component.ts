6e0193719d25ec20c25a068eff610017aa977d92
jwh1550 <j0861550@gmail.com>
2023-02-22 13:14:35 +0100

    Removed some stuff

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
-})
-export class AboutComponent {
-  title = "Tagger"
-  version = '0.5.0';
+// @Component({
+//   selector: 'app-about',
+//   templateUrl: './about.component.html',
+//   styleUrls: ['./about.component.scss']
+// })
+// export class AboutComponent {
+//   title = "Tagger"
+//   version = '0.5.0';
 
-}
+// }
