03022abb9df4f490c6d69aec7112fffb775647fe
jwh1550 <j0861550@gmail.com>
2023-02-23 18:34:02 +0100

    Removed save-tag

--- src/app/pages/popup/popup.component.ts
+++ src/app/pages/popup/popup.component.ts
@@ -1,4 +1,7 @@
+///<reference types="chrome"/>
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DbService, TagItem } from 'src/app/db.service';
 
 @Component({
   selector: 'app-popup',
@@ -6,5 +9,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./popup.component.scss']
 })
 export class PopupComponent {
+  constructor(private route: ActivatedRoute, private db: DbService) { }
+  save: boolean = true;
+  tagItems: TagItem[] = [];
 
+
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      this.db.TagItem.orderBy('id').reverse().first().then((maxTagItem: TagItem | undefined) => {
+        const maxId: number = maxTagItem ? maxTagItem.id : 0;
+        this.tagItems = [{
+          id: maxId + 1,
+          tags: [],
+          time: new Date().toLocaleString(),
+          selection: params['selectedText'],
+          url: params['url'],
+          title: params['title'],
+          metaDescription: params['metaDescription'],
+          isEditing: false,
+          isFlipped: false,
+          isShowing: true
+        }];
+      });
+    });
+  }
 }
