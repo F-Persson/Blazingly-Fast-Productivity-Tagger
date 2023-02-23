a9947cad4ac9d4cd4ca89d48e23e3ef8a3ee7abd
jwh1550 <j0861550@gmail.com>
2023-02-23 09:46:54 +0100

    moved about

--- src/app/pages/options/options.component.ts
+++ src/app/pages/options/options.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { liveQuery } from 'dexie';
 import { DbService, TagItem } from 'src/app/db.service';
+import { aboutItem } from 'src/app/about';
 
 
 @Component({
@@ -23,42 +24,8 @@ export class OptionsComponent {
   hasSearchResults = false;
   flipall: boolean = false;
   showAbout: boolean = false;
+  aboutItem: TagItem[] = aboutItem;
 
-  aboutItem: TagItem[] = [
-    {
-      title: "Hi, my name is Francis, I'm a software developer",
-      selection: "I developed this extension to improve my software skills and learn Angular.\n\nI hope that you also find it helpful.",
-      url: "https://fpersson.com",
-      id: 0,
-      tags: ["about", "contact", "help", "feedback"],
-      time: new Date().toLocaleString(),
-      isEditing: false,
-      isFlipped: false,
-      isShowing: true,
-    },
-    {
-      title: "TODO:",
-      selection: "Make backend in C# for saving and sharing your tagItems\n\nA login option\n\nFor more info please see the github repo.\nYou'll find my contact info at the bottom of this page.",
-      url: "https://github.com/F-Persson",
-      id: -1,
-      tags: ["Tagger", "Version 0.5", "Open Source", "feedback"],
-      time: new Date().toLocaleString(),
-      isEditing: false,
-      isFlipped: false,
-      isShowing: true,
-    },
-    {
-      title: "How to",
-      selection: "On any page:\n\n select some text (optional) and press ctrl + left click to save the page\n\npress ctrl + right click to open this option page\n\nClick the text (this text) to flip any card",
-      url: "https://tothechromeextensionurl.com",
-      id: -2,
-      tags: ["Instructions", "Info", "Howto"],
-      time: new Date().toLocaleString(),
-      isEditing: false,
-      isFlipped: true,
-      isShowing: true,
-    }
-  ];
 
   async exportItemsToJson() {
     // get all items from db
