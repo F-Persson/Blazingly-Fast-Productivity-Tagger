438a7c140a4acde736bb65954586eb6bffafc026
F-Persson <persson_f@outlook.com>
2023-03-27 13:32:20 +0200

    .

--- src/app/services/backend-service.service.ts
+++ src/app/services/backend-service.service.ts
@@ -2,16 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { TagItem } from 'src/app/services/db.service';
 
-// export interface ApiItem {
-//   tagId: number;
-//   tags: string[];
-//   time: string;
-//   selection: string;
-//   url: string;
-//   title: string;
-//   metaDescription?: string;
-// };
-
 @Injectable()
 export class BackendService {
   tagId!: number;
@@ -23,7 +13,7 @@ export class BackendService {
   metaDescription?: string;
 
   constructor(private http: HttpClient) { }
-  private readonly URL = "https://localhost:7293/api/TagItems";
+  private readonly URL = "https://blazingapi.azurewebsites.net/api/tagitems";
 
 
   getAll(): any {
@@ -31,15 +21,6 @@ export class BackendService {
   }
 
   postData(data: TagItem): any {
-    // const apiItem: ApiItem = {
-    //   tagId: data.id,
-    //   tags: data.tags,
-    //   time: data.time,
-    //   selection: data.selection,
-    //   url: data.url,
-    //   title: data.title,
-    //   metaDescription: data.metaDescription
-    // };
 
     const options = {
       headers: {
